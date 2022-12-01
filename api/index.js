5d0463452959e2885a824c3f5643aa6d6af30f1e
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-12-01 14:40:51 -0300

    async

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const {dataBaseLoader,genreLoader}=require("./src/utils/dbloader")
 // const PUERTO= PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ alter: true }).then(async() => {
     server.listen(process.env.PORT, () => {
   console.log(`%s listening at ${process.env.PORT} ` ); // eslint-disable-line no-console
 });
